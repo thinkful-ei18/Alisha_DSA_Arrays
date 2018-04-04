1a4baa53749747a96ca23242a32d16f7917a2324
alishaevn <alieva.inc@gmail.com>
2018-04-04 09:02:47 -0700

    outlined the class

---
+++ Array.js
@@ -0,0 +1,51 @@
+'use strict';
+const Memory = require('./Memory');
+
+/*
+Walk through each step of implementing an array. Don't rush through this by copying and pasting the code 
+samples. Once you've walked through it and you understand the code of the Array class, hide the sample 
+code and try writing the array class from scratch using the memory module here for allocating memory.
+
+If you are using node, export the memory module and then import it using require. If you are using 
+React, import the module using import*
+*/
+
+class Array {
+
+  constructor() {
+    
+  }
+  
+
+  get() {
+
+  }
+
+
+  resize() {
+
+  }
+
+
+  push() {
+
+  }
+
+  
+  pop() {
+
+  }
+
+
+  insert() {
+
+  }
+
+
+  remove() {
+
+  }
+
+}
+
+module.exports = Array;
